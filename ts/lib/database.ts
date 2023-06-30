cf60293ee13ecd6a51796d116f85fc1d7f6dc49b
k.mitarai <kazmit299@gmail.com>
2023-06-30 21:50:52 +0900

    DB抽象化。sqliteをローカル運用用のDBとして動くように追加

--- ts/lib/database.ts
+++ ts/lib/database.ts
@@ -1,24 +1,8 @@
-const mariadb = require('mariadb');
-const pool = mariadb.createPool({host: "localhost", user: process.env.DB_USER, password: process.env.DB_PWD, connectionLimit: 5});
-let conn: any = null;
-
-async function connect()
-{
-  if(conn) return ;
-  
-  conn = await mariadb.createConnection({
-      host: "localhost",
-      user: process.env.DB_USER,
-      password: process.env.DB_PWD,
-      database: "vtn-game-sample"
-      //timezone: 'Asia/Tokyo',
-      //skipSetTimezone: true
-  });
-}
+//DBを選択
+//import { execQuery } from "./dbmodule/mariadb";
+import { execQuery } from "./dbmodule/sqlite3";
 
 export async function query(query: string, values: any)
 {
-  await connect();
-  const res = await conn.query(query, values);
-  return res;
+  return await execQuery(query, values);
 }
